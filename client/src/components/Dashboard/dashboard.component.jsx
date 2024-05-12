5fb6bdb95e2a6e659cdeb071971739309a883aa0
r1tikpatil <ritikpatil566@gmail.com>
2024-05-12 18:22:18 +0530

    feat: add search functionality

--- client/src/components/Dashboard/dashboard.component.jsx
+++ client/src/components/Dashboard/dashboard.component.jsx
@@ -8,6 +8,7 @@ import Loader from "../../common/components/LoaderComponent/loader.component";
 const Dashboard = () => {
   const navigate = useNavigate();
   const [allPatientsList, setAllPatientsList] = useState([]);
+  const [filterPatientsList, setFilterPatientsList] = useState([]);
   const [loading, setLoading] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -23,7 +24,10 @@ const Dashboard = () => {
     setLoading(true);
     try {
       const res = await getAllPatients();
-      if (res.success) setAllPatientsList(res.data);
+      if (res.success) {
+        setAllPatientsList(res.data);
+        setFilterPatientsList(res.data);
+      }
     } catch (err) {
       console.log(err);
     }
@@ -46,17 +50,40 @@ const Dashboard = () => {
     openModal();
   };
 
+  const handleSearch = (e) => {
+    const searchValue = e.target.value.toLowerCase();
+
+    if (allPatientsList && setFilterPatientsList) {
+      const data = allPatientsList.filter(
+        (item) =>
+          item.firstName.toLowerCase().includes(searchValue) ||
+          item.lastName.toLowerCase().includes(searchValue) ||
+          item.email.toLowerCase().includes(searchValue)
+      );
+
+      setFilterPatientsList(data);
+    }
+  };
+
   return (
     <div className="w-full flex items-center justify-center">
       <div className="mx-20 min-w-[50%] max-w-[80%]">
         <div className=" my-4 flex justify-between">
-          <span className="text-2xl">Patient List</span>
+          <span className="text-2xl mr-16">Patient List</span>
           <button
             onClick={handleAddPatient}
             className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded mb-4"
           >
             Add Patient
           </button>
+        </div>
+        <div className="my-3">
+          <input
+            type="text"
+            onChange={handleSearch}
+            placeholder="search patient by name or email Id"
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-1  px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          />
         </div>
         {isModalOpen && <AddPatientModal closeModal={closeModal} />}
         {!loading ? (
@@ -91,7 +118,7 @@ const Dashboard = () => {
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
-                {allPatientsList.map((patient) => (
+                {filterPatientsList.map((patient) => (
                   <tr key={patient.patientId}>
                     <td className="px-6 py-4 whitespace-nowrap">
                       <div className="flex items-center">
