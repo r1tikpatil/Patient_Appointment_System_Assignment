291580010dfdf79b467e353ed16e5ebde70f9391
r1tikpatil <ritikpatil566@gmail.com>
2024-05-12 10:45:53 +0530

    update: appointment model

--- server/app/models/appointment_model.py
+++ server/app/models/appointment_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, TIMESTAMP
+from sqlalchemy import Column, Integer, TIMESTAMP, BOOLEAN
 from . import Base
 from typing import Any
 from app.utils.utility import to_dict
@@ -11,6 +11,7 @@ class Appointment(Base):
     patientId = Column(Integer)
     date = Column(TIMESTAMP)
     amount = Column(Integer)
+    isPaid = Column(BOOLEAN, default=False)
 
     def dict(self) -> dict[str, Any]:
         return to_dict(self)
